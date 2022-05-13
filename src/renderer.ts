3763b1f347bc01694f162c5f01d0f3c0457f9a4e
ElijahMSmith <mcditto99@gmail.com>
2022-05-12 23:24:11 -0400

    Started electron setup

---
+++ src/renderer.ts
@@ -0,0 +1,13 @@
+// Since we declared the script as type=module in the HTML file,
+// we can use ES Modules (adapted from the Vue 2 Introduction
+// https://vuejs.org/v2/guide/#Declarative-Rendering
+
+// Alternatively, omit the .min from the path for Vue debugging purposes.
+import Vue from "../node_modules/vue/dist/vue.esm-browser.js";
+
+const app = new Vue({
+	el: "#app",
+	data: {
+		message: "Hello Vue!",
+	},
+});
